d652b8ea4144d4d0490ef9579c6b4dcc6510af0e
Gediminas Luksas <gediminasluksas@gmail.com>
2019-10-30 09:46:37 +0200

    Reducer and displaying postsList from api

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,5 +3,5 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fethPosts = () => async dispath => {
         const response = await jsonPlaceholder.get('/posts');
     
-      dispath({ type: 'FETCH_POSTS', payload: response })  
+      dispath({ type: 'FETCH_POSTS', payload: response.data })  
     };
